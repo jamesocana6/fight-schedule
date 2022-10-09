1979bba8e3829763585c89c5933fb1a86f30be48
James Ocana <jamesocana@jamess-air.myfiosgateway.com>
2022-10-08 23:23:27 -0400

    connected bellatore and one

---
+++ scrapers/ufc-espn.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const URL = "https://www.espn.com/mma/schedule/_/league/ufc"
+const fights = [];
+const getData = async () => {
+    const {data} = await axios.get(URL)
+    const $ = cheerio.load(data)
+    const headline = $(".event__col");
+    const event = $(".date__innerCell");
+    const time = $(".date__col a.AnchorLink")
+    // Use .each method to loop through the elemtns we selected
+    headline.each((idx, el) => {
+        const fight = {index:"", event: ""};
+        fight.event = $(el).children("a").text();
+        fight.index = idx;
+        fights.push(fight);
+    });
+    event.each((idx, el) => {
+        const fight = {date: ""};
+        fight.date = $(el).text();
+        if (fight.date != undefined) {
+            fights[idx]["date"] = fight.date;
+        }
+    });
+    time.each((idx, el) => {
+        const fight = {time: ""};
+        fight.time = $(el).text();
+        if (fight.time != undefined) {
+            try {
+                fights[idx]["time"] = fight.time;
+            } catch {
+
+            }
+        }
+    });
+    console.log("These are fights: ", fights)
+}
+
+getData()
+
+module.exports = fights;
--- server.js
+++ server.js
@@ -1,49 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 4000;
-const axios = require("axios");
-const cheerio = require("cheerio");
-
-const URL = "https://www.espn.com/mma/schedule/_/league/ufc"
-const getData = async () => {
-    const {data} = await axios.get(URL)
-    const $ = cheerio.load(data)
-    const headline = $(".event__col");
-    const event = $(".date__innerCell");
-    const time = $(".date__col a.AnchorLink")
-    const fights = [];
-    // Use .each method to loop through the elemtns we selected
-    headline.each((idx, el) => {
-        const fight = {index:"", event: ""};
-        fight.event = $(el).children("a").text();
-        fight.index = idx;
-        fights.push(fight);
-    });
-    event.each((idx, el) => {
-        const fight = {date: ""};
-        fight.date = $(el).text();
-        if (fight.date != undefined) {
-            fights[idx]["date"] = fight.date;
-        }
-    });
-    time.each((idx, el) => {
-        const fight = {time: ""};
-        fight.time = $(el).text();
-        if (fight.time != undefined) {
-            try {
-                fights[idx]["time"] = fight.time;
-            } catch {
-
-            }
-        }
-    });
-    console.log("These are fights: ", fights)
-}
+// const axios = require("axios");
+// const cheerio = require("cheerio");
+const UFC = require("./scrapers/ufc-espn.js");
+const BELL = require("./scrapers/bellatore-espn.js");
+const ONE = require("./scrapers/one-intwwe.js");
 
 
 app.get('/', (req, res) => {
-    getData();
-    res.send("test")
+    console.log(ONE)
+    console.log(BELL)
+    res.send(UFC)
 
 })
 
